17553455efcfc135179d25a8442d08b9c1a4d263
iberdinsky-skilld <iberdinsky@skilld.cloud>
2024-11-03 18:48:26 +0100

    Fixed schema import

---
+++ scripts/copyFiles.js
@@ -0,0 +1,7 @@
+import { copyFileSync } from 'node:fs'
+import { resolve } from 'node:path'
+
+copyFileSync(
+  resolve(__dirname, "../src/uiPatternsSchema.yml"),
+  resolve(__dirname, "../dist/uiPatternsSchema.yml")
+);
